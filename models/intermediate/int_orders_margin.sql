4b30e80a3f064a46491f03db366966a13b60c1ca
orphee.duriez@outlook.fr <orphee.duriez@outlook.fr>
2025-05-13 13:02:56 +0000

    Update challenge 4

--- models/intermediate/int_orders_margin.sql
+++ models/intermediate/int_orders_margin.sql
@@ -1,6 +1,7 @@
 WITH sales_data AS (
 SELECT 
-    sales.products_id,
+    sales.orders_id,
+    sales.date_date,
     products.purchase_price,
     sales.revenue, 
     sales.quantity,
@@ -11,5 +12,12 @@ LEFT JOIN {{ ref('stg_raw_data__product')}} AS products
 ON sales.products_id=products.products_id
 )
 
-SELECT *
+SELECT 
+orders_id,
+DATE(TIMESTAMP_SECONDS(CAST(AVG(UNIX_SECONDS(CAST(date_date AS TIMESTAMP))) AS INT64))) AS avg_date,
+SUM(revenue) AS revenue,
+SUM(quantity) AS quantity,
+SUM(purchase_cost) AS purchase_cost,
+SUM(margin) AS margin
 FROM sales_data
+GROUP BY orders_id
