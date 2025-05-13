81cc8ea2cba39ab8933e63e244d60ce6a15fba9f
orphee.duriez@outlook.fr <orphee.duriez@outlook.fr>
2025-05-13 12:49:09 +0000

    Update challenge 4

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -1,11 +1,15 @@
 WITH sales_data AS (
 SELECT 
-    sales.products_id
-    product.purchase_price,
+    sales.products_id,
+    products.purchase_price,
     sales.revenue, 
     sales.quantity,
+    (quantity * purchase_price) AS purchase_cost,
+    (revenue-(quantity * purchase_price)) AS margin
 FROM {{ ref('stg_raw_data__sales')}} AS sales
-LEFT JOIN {{ ref('stg_raw_data__product')}} AS product
-USING(products_id)
+LEFT JOIN {{ ref('stg_raw_data__product')}} AS products
+ON sales.products_id=products.products_id
 )
 
+SELECT *
+FROM sales_data
