5b32e2c58520d91f4dc65360a38afc7442e2cec0
orphee.duriez@outlook.fr <orphee.duriez@outlook.fr>
2025-05-14 12:44:01 +0000

    Update Challenge 4 end - 140525

--- models/intermediate/int_campaigns.sql
+++ models/intermediate/int_campaigns.sql
@@ -1,17 +1,3 @@
-SELECT *
-FROM {{ref("stg_raw_data__adwords")}}
-
-UNION ALL
-
-SELECT *
-FROM {{ref("stg_raw_data__bing")}}
-
-UNION ALL
-
-SELECT *
-FROM {{ref("stg_raw_data__facebook")}}
-
-UNION ALL
-
-SELECT *
-FROM {{ref("stg_raw_data__criteo")}}
+{{ dbt_utils.union_relations(
+    relations=[ref('stg_raw_data__adwords'), ref('stg_raw_data__facebook'), ref('stg_raw_data__criteo'), ref('stg_raw_data__bing')],
+) }}
